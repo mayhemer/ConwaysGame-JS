108bb94b9bdaa1a5a7c49a9a939693b69add575f
Honza Bambas <honzab@firemni.cz>
2024-05-24 16:26:10 +0200

    formatting

--- conwaysgame.js
+++ conwaysgame.js
@@ -1,122 +1,120 @@
 class ConwayWorld extends Uint8ClampedArray {
-    #BITS_PER_ELEMENT = super.BYTES_PER_ELEMENT * 8;
-    touched = false;
+  #BITS_PER_ELEMENT = super.BYTES_PER_ELEMENT * 8;
+  touched = false;
 
-    constructor(w, h) {
-        super((w * h) / 8);
-        this.width = w;
-        this.height = h;
+  constructor(w, h) {
+    super((w * h) / 8);
+    this.width = w;
+    this.height = h;
+  }
+
+  #index(x, y) {
+    const m = x + y * this.width;
+    const i = (m / this.#BITS_PER_ELEMENT) | 0;
+    const j = m % this.#BITS_PER_ELEMENT;
+    return { i, j };
+  }
+
+  #isOutbound(x, y) {
+    return x < 0 || x >= this.width || y < 0 || y >= this.height;
+  }
+
+  setCell(x, y, alive) {
+    if (this.#isOutbound(x, y)) {
+      return;
     }
 
-    #index(x, y) {
-        const m = (x + (y * this.width));
-        const i = (m / this.#BITS_PER_ELEMENT) | 0;
-        const j = m % this.#BITS_PER_ELEMENT; 
-        return {i, j};
+    const { i, j } = this.#index(x, y);
+    if (alive) {
+      this[i] |= 1 << j;
+      this.touched = true;
+    } else {
+      this[i] &= ~(1 << j);
+    }
+  }
+
+  getCell(x, y) {
+    if (this.#isOutbound(x, y)) {
+      return false;
     }
 
-    #isOutbound(x, y) {
-        return x < 0 || x >= this.width || y < 0 || y >= this.height;
+    const { i, j } = this.#index(x, y);
+    return !!(this[i] & (1 << j));
+  }
+
+  neigbors(x, y) {
+    let n = 0;
+    for (let a = x - 1; a <= x + 1; ++a) {
+      for (let b = y - 1; b <= y + 1; ++b) {
+        if (a == x && b == y) continue;
+        n += this.getCell(a, b) | 0;
+        if (n > 3) break;
+      }
     }
+    return n;
+  }
 
-    setCell(x, y, alive) {
-        if (this.#isOutbound(x, y)) {
-            return;
-        }
-        
-        const {i, j} = this.#index(x, y);
-        if (alive) {
-            this[i] |= (1 << j);
-            this.touched = true;
-        } else {
-            this[i] &= ~(1 << j); 
-        }
-    }
-
-    getCell(x, y) {
-        if (this.#isOutbound(x, y)) {
-            return false;
-        }
-
-        const {i, j} = this.#index(x, y);
-        return !!(this[i] & (1 << j));
-    }
-
-    neigbors(x, y) {
-        let n = 0;
-        for (let a = x - 1; a <= x + 1; ++a) {
-            for (let b = y - 1; b <= y + 1; ++b) {
-                if (a == x && b == y) continue;
-                n += this.getCell(a, b) | 0;
-                if (n > 3) break;
-            }
-        }
-        return n;
-    }
-
-    nextGen() {
-        let r = new ConwayWorld(this.width, this.height);
-        for (let x = 0; x < this.width; ++x) {
-            for (let y = 0; y < this.height; ++y) {
-                const c = this.getCell(x, y);
-                const n = this.neigbors(x, y);
-                /*
+  nextGen() {
+    let r = new ConwayWorld(this.width, this.height);
+    for (let x = 0; x < this.width; ++x) {
+      for (let y = 0; y < this.height; ++y) {
+        const c = this.getCell(x, y);
+        const n = this.neigbors(x, y);
+        /*
                 Rule 1: a dead cell with exactly 3 neighbours comes alive
                 Rule 2: an alive cell with less than 2 or more than 3 neighbours dies
                 */
-                if (c ? n >= 2 && n <= 3 : n == 3) {
-                    r.setCell(x, y, true);
-                }
-            }
+        if (c ? n >= 2 && n <= 3 : n == 3) {
+          r.setCell(x, y, true);
         }
+      }
+    }
 
-        return r;
-    }    
+    return r;
+  }
 }
 
 class ConwayWorldGame {
-    static #randomInit(world) {
-        world.forEach((e, i) => {
-            const a = Math.random() * 256;
-            const b = Math.random() * 256;
-            world[i] = a && b; // to sparse...
-        });
+  static #randomInit(world) {
+    world.forEach((e, i) => {
+      const a = Math.random() * 256;
+      const b = Math.random() * 256;
+      world[i] = a && b; // to sparse...
+    });
+  }
+
+  static #paint(world, next, target) {
+    for (let x = 0; x < world.width; ++x) {
+      for (let y = 0; y < world.height; ++y) {
+        target(x, y, world.getCell(x, y), next.getCell(x, y));
+      }
     }
-    
-    static #paint(world, next, target) {
-        for (let x = 0; x < world.width; ++x) {
-            for (let y = 0; y < world.height; ++y) {
-                target(x, y, world.getCell(x, y), next.getCell(x, y));
-            }
+  }
+
+  static #progress(world, target) {
+    const next = world.nextGen();
+    this.#paint(world, next, target);
+    return next;
+  }
+
+  static loop(width, height, target, interval) {
+    const w0 = new ConwayWorld(width, height);
+    let world = new ConwayWorld(width, height);
+
+    this.#randomInit(world);
+    this.#paint(w0, world, target);
+
+    return new Promise((resolve) => {
+      const handle = setInterval(() => {
+        world = this.#progress(world, target);
+
+        if (!world.touched) {
+          clearInterval(handle);
+          console.log("All dead...");
+          resolve();
+          return;
         }
-        
-    }
-    
-    static #progress(world, target) {
-        const next = world.nextGen();
-        this.#paint(world, next, target);
-        return next;
-    }
-    
-    static loop(width, height, target, interval) {
-        const w0 = new ConwayWorld(width, height);
-        let world = new ConwayWorld(width, height);
-
-        this.#randomInit(world);
-        this.#paint(w0, world, target);
-        
-        return new Promise(resolve => {
-            const handle = setInterval(() => {
-                world = this.#progress(world, target);
-                
-                if (!world.touched) {
-                    clearInterval(handle);
-                    console.log('All dead...');
-                    resolve();
-                    return;
-                }
-
-            }, interval);
-        });
-    }    
+      }, interval);
+    });
+  }
 }
--- unittest.js
+++ unittest.js
@@ -1,95 +1,100 @@
 (function conwaySelfTest() {
-    const check = (cond, msg = "Failure") => { if (!cond) throw new Error(msg); }
+  const check = (cond, msg = "Failure") => {
+    if (!cond) throw new Error(msg);
+  };
 
-    console.log('unittest begin');
+  console.log("unittest begin");
 
-    // simple arithmetics of the bit array
-    let w = new ConwayWorld(5, 5);
-    check(!w.touched);
-    w.setCell(3, 3, false);
-    check(!w.touched);
-    w.setCell(3, 3, true);
-    check(w.touched);
-    check(w.getCell(3, 3));
-    check(!w.getCell(2, 2));
-    check(!w.getCell(2, 3));
-    check(!w.getCell(3, 2));
-    check(!w.getCell(4, 2));
+  // simple arithmetics of the bit array
+  let w = new ConwayWorld(5, 5);
+  check(!w.touched);
+  w.setCell(3, 3, false);
+  check(!w.touched);
+  w.setCell(3, 3, true);
+  check(w.touched);
+  check(w.getCell(3, 3));
+  check(!w.getCell(2, 2));
+  check(!w.getCell(2, 3));
+  check(!w.getCell(3, 2));
+  check(!w.getCell(4, 2));
 
-    // test the neighbours function
-    check(w.neigbors(1, 1) == 0);
-    check(w.neigbors(2, 2) == 1);
-    check(w.neigbors(4, 2) == 1);
-    check(w.neigbors(2, 3) == 1);
-    check(w.neigbors(3, 3) == 0);
-    check(w.neigbors(4, 3) == 1);
-    check(w.neigbors(2, 4) == 1);
-    check(w.neigbors(3, 4) == 1);
-    check(w.neigbors(4, 4) == 1);
-    
-    // only one cell alive means next gen is all-dead
-    let w2 = w.nextGen();
-    check(w.getCell(3, 3));
-    check(!w2.getCell(3, 3));
-    check(!w2.touched);
+  // test the neighbours function
+  check(w.neigbors(1, 1) == 0);
+  check(w.neigbors(2, 2) == 1);
+  check(w.neigbors(4, 2) == 1);
+  check(w.neigbors(2, 3) == 1);
+  check(w.neigbors(3, 3) == 0);
+  check(w.neigbors(4, 3) == 1);
+  check(w.neigbors(2, 4) == 1);
+  check(w.neigbors(3, 4) == 1);
+  check(w.neigbors(4, 4) == 1);
 
-    // this pattern oscilates (a 3 cell horizontal bar)
-    w.setCell(1, 3, true);
-    w.setCell(2, 3, true);
-    w.setCell(3, 3, true);
-    let w3 = w.nextGen();
-    for (let x = 0; x < 5; ++x) for (let y = 0; y < 5; ++y) {
-        check(w3.getCell(x, y) == (
-            x == 2 && y == 2 ||
-            x == 2 && y == 3 ||
-            x == 2 && y == 4
-        ), `altering forth x=${x} y=${y} n=${w3.neigbors(x, y)}`);
+  // only one cell alive means next gen is all-dead
+  let w2 = w.nextGen();
+  check(w.getCell(3, 3));
+  check(!w2.getCell(3, 3));
+  check(!w2.touched);
+
+  // this pattern oscilates (a 3 cell horizontal bar)
+  w.setCell(1, 3, true);
+  w.setCell(2, 3, true);
+  w.setCell(3, 3, true);
+  let w3 = w.nextGen();
+  for (let x = 0; x < 5; ++x)
+    for (let y = 0; y < 5; ++y) {
+      check(
+        w3.getCell(x, y) ==
+          ((x == 2 && y == 2) || (x == 2 && y == 3) || (x == 2 && y == 4)),
+        `altering forth x=${x} y=${y} n=${w3.neigbors(x, y)}`
+      );
     }
 
-    // ...and try it back
-    let w4 = w3.nextGen();
-    for (let x = 0; x < 5; ++x) for (let y = 0; y < 5; ++y) {
-        check(w4.getCell(x, y) == (
-            x == 1 && y == 3 ||
-            x == 2 && y == 3 ||
-            x == 3 && y == 3
-        ), `altering back x=${x} y=${y}`);
+  // ...and try it back
+  let w4 = w3.nextGen();
+  for (let x = 0; x < 5; ++x)
+    for (let y = 0; y < 5; ++y) {
+      check(
+        w4.getCell(x, y) ==
+          ((x == 1 && y == 3) || (x == 2 && y == 3) || (x == 3 && y == 3)),
+        `altering back x=${x} y=${y}`
+      );
     }
 
-    // this has to remain stable (a 3 by 3 pointed star)
-    let w0 = new ConwayWorld(5, 5);
-    w0.setCell(2, 1, true);
-    w0.setCell(2, 3, true);
-    w0.setCell(1, 2, true);
-    w0.setCell(3, 2, true);
-    let w1 = w0.nextGen();
-    for (let x = 0; x < 5; ++x) for (let y = 0; y < 5; ++y) {
-        check(w1.getCell(x, y) == w0.getCell(x, y), `stable x=${x} y=${y}`);
+  // this has to remain stable (a 3 by 3 pointed star)
+  let w0 = new ConwayWorld(5, 5);
+  w0.setCell(2, 1, true);
+  w0.setCell(2, 3, true);
+  w0.setCell(1, 2, true);
+  w0.setCell(3, 2, true);
+  let w1 = w0.nextGen();
+  for (let x = 0; x < 5; ++x)
+    for (let y = 0; y < 5; ++y) {
+      check(w1.getCell(x, y) == w0.getCell(x, y), `stable x=${x} y=${y}`);
     }
-    
-    // check leaks to other side
-    let wn = new ConwayWorld(6, 8);
-    wn.setCell(0, 2, true);
-    wn.setCell(0, 3, true);
-    wn.setCell(0, 4, true);
-    wn.setCell(0, 5, true);
-    wn.setCell(0, 6, true);
-    check(!wn.neigbors(5, 0));
-    check(!wn.neigbors(5, 1));
-    check(!wn.neigbors(5, 2));
-    check(!wn.neigbors(5, 3));
-    check(!wn.neigbors(5, 4));
-    wn = wn.nextGen(); 
-    check(!wn.getCell(5, 0));
-    check(!wn.getCell(5, 1));
-    check(!wn.getCell(5, 2));
-    check(!wn.getCell(5, 3));
-    check(!wn.getCell(5, 4));
-    check(!wn.neigbors(5, 0));
-    check(!wn.neigbors(5, 1));
-    check(!wn.neigbors(5, 2));
-    check(!wn.neigbors(5, 3));
-    check(!wn.neigbors(5, 4));
 
-    console.log('unittest end');
+  // check leaks to other side
+  let wn = new ConwayWorld(6, 8);
+  wn.setCell(0, 2, true);
+  wn.setCell(0, 3, true);
+  wn.setCell(0, 4, true);
+  wn.setCell(0, 5, true);
+  wn.setCell(0, 6, true);
+  check(!wn.neigbors(5, 0));
+  check(!wn.neigbors(5, 1));
+  check(!wn.neigbors(5, 2));
+  check(!wn.neigbors(5, 3));
+  check(!wn.neigbors(5, 4));
+  wn = wn.nextGen();
+  check(!wn.getCell(5, 0));
+  check(!wn.getCell(5, 1));
+  check(!wn.getCell(5, 2));
+  check(!wn.getCell(5, 3));
+  check(!wn.getCell(5, 4));
+  check(!wn.neigbors(5, 0));
+  check(!wn.neigbors(5, 1));
+  check(!wn.neigbors(5, 2));
+  check(!wn.neigbors(5, 3));
+  check(!wn.neigbors(5, 4));
+
+  console.log("unittest end");
 })();
