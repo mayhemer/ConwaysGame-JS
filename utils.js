658042336bd760c301bef02fadd1c4df4119ae8f
Honza Bambas <honzab@firemni.cz>
2024-05-25 22:45:01 +0200

    utils for enlarge, shring

--- utils.js
+++ utils.js
@@ -4,9 +4,10 @@ class ConwayWorldUtils {
       hx = 0,
       ly = world.width,
       hy = 0;
+
     for (let x = 0; x < world.width; ++x) {
       for (let y = 0; y < world.height; ++y) {
-        if (world.getCell(x,y)) {
+        if (world.getCell(x, y)) {
           if (hx < x) hx = x;
           if (lx > x) lx = x;
           if (hy < y) hy = y;
@@ -14,6 +15,7 @@ class ConwayWorldUtils {
         }
       }
     }
+
     const ax = (hx + lx) / 2;
     const ay = (hy + ly) / 2;
     const dx = world.width / 2 - ax;
@@ -22,8 +24,40 @@ class ConwayWorldUtils {
     const r = new ConwayWorld(world.width, world.height);
     for (let x = 0; x < world.width; ++x) {
       for (let y = 0; y < world.height; ++y) {
-        if (world.getCell(x,y)) {
-          r.setCell(x+dx, y+dy, true);
+        if (world.getCell(x, y)) {
+          r.setCell(x + dx, y + dy, true);
+        }
+      }
+    }
+
+    return r;
+  }
+
+  static enlarge(world) {
+    const r = new ConwayWorld(world.width * 2, world.height * 2);
+    const dx = world.width / 2;
+    const dy = world.height / 2;
+
+    for (let x = 0; x < world.width; ++x) {
+      for (let y = 0; y < world.height; ++y) {
+        if (world.getCell(x, y)) {
+          r.setCell(x + dx, y + dy, true);
+        }
+      }
+    }
+
+    return r;
+  }
+
+  static shrink(world) {
+    const r = new ConwayWorld(world.width / 2, world.height / 2);
+    const dx = world.width / 4;
+    const dy = world.height / 4;
+
+    for (let x = dx; x < dx * 3; ++x) {
+      for (let y = dy; y < dy * 3; ++y) {
+        if (world.getCell(x, y)) {
+          r.setCell(x - dx, y - dy, true);
         }
       }
     }
